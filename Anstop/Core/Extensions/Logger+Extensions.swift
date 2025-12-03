d1f7d4b65c946f9303233179a87dca0801525985
MoLines Designs <admin@molinesdesigns.com>
2025-12-03 22:01:01 +0100

    actualización general

--- Anstop/Core/Extensions/Logger+Extensions.swift
+++ Anstop/Core/Extensions/Logger+Extensions.swift
@@ -1,9 +1,9 @@
 import OSLog
 
 extension Logger {
-    nonisolated(unsafe) static let ui = Logger(subsystem: "com.molinesdesigns.Anstop", category: "ui")
-    nonisolated(unsafe) static let network = Logger(subsystem: "com.molinesdesigns.Anstop", category: "network")
-    nonisolated(unsafe) static let database = Logger(subsystem: "com.molinesdesigns.Anstop", category: "database")
-    nonisolated(unsafe) static let analytics = Logger(subsystem: "com.molinesdesigns.Anstop", category: "analytics")
-    nonisolated(unsafe) static let purchases = Logger(subsystem: "com.molinesdesigns.Anstop", category: "purchases")
+    static let ui = Logger(subsystem: "com.molinesdesigns.Anstop", category: "ui")
+    static let network = Logger(subsystem: "com.molinesdesigns.Anstop", category: "network")
+    static let database = Logger(subsystem: "com.molinesdesigns.Anstop", category: "database")
+    static let analytics = Logger(subsystem: "com.molinesdesigns.Anstop", category: "analytics")
+    static let purchases = Logger(subsystem: "com.molinesdesigns.Anstop", category: "purchases")
 }
